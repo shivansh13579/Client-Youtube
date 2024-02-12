f85c3bb50dc264c60b331e9ed3571d519342d0d7
shivansh13579 <shivansh0975@gmail.com>
2024-02-12 22:07:12 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,39 @@
+
+import { createRequire } from 'module'
+const require = createRequire(
+    import.meta.url);
+
+const express = require('express')
+const ytdl = require('ytdl-core')
+const cors = require('cors')
+
+require("dotenv").config();
+
+const port = process.env.PORT || 4000;
+
+const app = express()
+app.use(express.static('dist'))
+app.use(cors())
+
+
+
+app.get("/download", async(req, res) => {
+    try {
+        const url = req.query.url;
+        const videoId = await ytdl.getURLVideoID(url);
+        const metaInfo = await ytdl.getInfo(url);
+
+        let data = {
+            url: "https://www.youtube.com/embed/" + videoId,
+            info: metaInfo.formats
+        }
+
+        return res.send(data)
+    } catch (error) {
+        return res.status(500)
+    }
+})
+
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`)
+})
